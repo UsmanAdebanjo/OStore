ef61823fd4246201f1c5a0d8ad571341fdc81365
Usman Adebanjo <oluwatobilobalayeuthman@gmail.com>
2022-03-27 10:46:20 +0100

    Initial Commit

---
+++ API/Data/Migrations/20220323150735_InitialCorrect.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class InitialCorrect : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
